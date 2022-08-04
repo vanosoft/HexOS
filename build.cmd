97150b14e7b400b63b39e25f584b44e6784cf54c
TalismanChet <67498705+vanosoft@users.noreply.github.com>
2022-08-04 11:34:53 +0300

    Delete build.cmd

--- build.cmd
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-fasm kernel\kernel.asm
-fasm boot\boot.asm
-fasm boot\second.asm
-fasm collector.asm
-copy collector.bin image.bin /b
-copy collector.bin image.img /b
-del collector.bin /s /q
-echo Done. See file [image.img]
